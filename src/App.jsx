d96d74bce86a66a88543a4fafd2f654a52a6aa69
Kaio Brenner <kaiobrenner460@protonmail.com>
2023-06-30 10:01:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./assets/img/logo.png";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Home />
+  );
+};
+
+export default App;
